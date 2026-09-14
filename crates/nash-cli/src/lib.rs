93f88abf9d426536d07539b87b4ea905a6656b5c
rvcas <x@rvcas.dev>
2026-09-13 23:28:10 -0400

    chore: update repo link

--- crates/nash-cli/src/lib.rs
+++ crates/nash-cli/src/lib.rs
@@ -27,7 +27,7 @@ pub const BANNER: &str = color_print::cstr!(
 
  The <green><bold>Nash</bold></green> programming language.
 
- <magenta>repo:</magenta> <blue><italic><dim>https://github.com/nash-script/compiler</dim></italic></blue>
+ <magenta>repo:</magenta> <blue><italic><dim>https://github.com/orbistry/nash</dim></italic></blue>
  <magenta>docs:</magenta> <blue><italic><dim>https://nash-script.dev</dim></italic></blue>
  <magenta>chat:</magenta> <blue><italic><dim>https://discord.gg/3qQGrKT3eE</dim></italic></blue>"#
 );
